6eaf7f06ab4f38a4c55ec24d4272ae66441953a3
Lovastatina <cloverz6@outlook.com>
2023-08-11 11:49:38 -0600

    Se implemento salto

---
+++ Gam1/Assets/Scripts/rebotarebota.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rebotarebota : MonoBehaviour
+{
+    private Rigidbody2D Micuerpomisreglas;
+    public float Saltof = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Micuerpomisreglas = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            print("Se presionó salto");
+            Micuerpomisreglas.AddForce(
+                new Vector3(0, Saltof, 0),
+                ForceMode2D.Impulse);
+
+               
+        }
+    }
+}
