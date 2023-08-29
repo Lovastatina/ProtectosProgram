ea4aad07f9cfe16b3d501d59ea493eaaee5e0f7e
Lovastatina <cloverz6@outlook.com>
2023-08-29 14:54:01 -0600

    Equisde

---
+++ Plataformero/Assets/Script/Control.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+
+{
+    private Rigidbody2D MiCuerpo;
+    private Animator MiAnimador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MiCuerpo = GetComponent<Rigidbody2D>();
+        MiAnimador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velVert = MiCuerpo.velocity.y;
+        MiCuerpo.velocity = new Vector3(5, velVert, 0);
+   
+
+        MiAnimador.SetBool("Caminando", true);
+    }
+}
