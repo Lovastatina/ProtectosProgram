913fe9e44572678bd1a733f0f1fa6782d6798c78
Lovastatina <142038796+Lovastatina@users.noreply.github.com>
2023-09-21 15:05:49 -0600

    Realmente deberia suicidarme

---
+++ Plataformero/Assets/Script/CameraHell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHell : MonoBehaviour
+{
+    public Transform juegador;
+
+    void start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(juegador.position.x, juegador.position.y, -1);
+        
+    }
+}
