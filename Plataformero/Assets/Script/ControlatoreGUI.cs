0526596b1fab7067008167f0d468b87ffb5fcc4a
Lovastatina <142038796+Lovastatina@users.noreply.github.com>
2023-09-29 21:28:48 -0600

    Bene e finito

--- Plataformero/Assets/Script/ControlatoreGUI.cs
+++ Plataformero/Assets/Script/ControlatoreGUI.cs
@@ -8,17 +8,35 @@ public class ControlatoreGUI : MonoBehaviour
     public Personaggio Heroe;
     public Text EtiquetaHPHeroe;
     public Image BarraHPHeroe;
-    
+    public Text ViteHeroe;
+    public Text ScoreHeroe;
+
+    public Personaggio Villano;
+    public Text EtiquetaHPVillano;
+    public Image BarraHPVillano;
 
     // Aishitemaru L
     void Update()
     {
-        EtiquetaHPHeroe.text =
-            Heroe.hp + "/" + Heroe.hpMax;
+        EtiquetaHPHeroe.text = Heroe.hp + "/" + Heroe.hpMax;
 
         float percentHPHeroe = Heroe.hp / (float)Heroe.hpMax;
 
         BarraHPHeroe.fillAmount = percentHPHeroe;
+
+        ViteHeroe.text = Heroe.vite +"";
+
+        ScoreHeroe.text = "Score: " + Heroe.score + "";
+
+
+        EtiquetaHPVillano.text = Villano.hp + "/" + Villano.hpMax;
+
+        float percentHPVillano = Villano.hp / (float)Villano.hpMax;
+
+        BarraHPVillano.fillAmount = percentHPVillano;
+
+
+
     }
 
 
