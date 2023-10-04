3ec37f81c0f14d5467cf348c4f1c8d56802ce9a9
Lovastatina <142038796+Lovastatina@users.noreply.github.com>
2023-10-03 21:40:05 -0600

    Ya speedruntarea

--- Plataformero/Assets/Script/Personaggio.cs
+++ Plataformero/Assets/Script/Personaggio.cs
@@ -8,6 +8,8 @@ public class Personaggio : MonoBehaviour
     public int hpMax = 100;
     public int vite = 3;
     public int score = 0;
+    public GameObject sangue;
+    public GameObject persoVite;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +27,28 @@ public class Personaggio : MonoBehaviour
     {
         hp = hp - puntesDam;
         print(name + "recibe daño de :" + puntesDam + " por " + enemigue);
+
+        GameObject efettoSangue = Instantiate(sangue);
+        Personaggio elPer= GetComponent<Personaggio>();
+        efettoSangue.transform.position = elPer.transform.position;
+       
+        if(hp<=0)
+        {
+            vite = vite - 1;
+            GameObject efettoVite = Instantiate(persoVite);
+            efettoVite.transform.position = elPer.transform.position;
+        }
+
     }
 
     public void muerteInstantanea(GameObject who)
     {
         print(name + " murió instantaneamente por" + who);
         hp = 0;
+        Personaggio elPerr = GetComponent<Personaggio>();
+        vite = vite - 1;
+        GameObject efettoVite = Instantiate(persoVite);
+        efettoVite.transform.position = elPerr.transform.position;
     }
 
 }
